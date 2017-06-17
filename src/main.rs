e93686ce461e3b83024d74402347786317e87273
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2017-06-17 16:06:43 -0500

    the code that gets the prices of crypto currencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate json;
+extern crate reqwest;
+
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut json_body = String::new();
+    let mut res = reqwest::get("https://api.coinmarketcap.com/v1/ticker/?limit=10").unwrap();
+
+    assert!(res.status().is_success());
+
+    res.read_to_string(&mut json_body);
+
+    let parsed = json::parse(&json_body).unwrap();
+
+    // members() is used to iterate over an array
+    for item in parsed.members() {
+        // need to first extract the string from the json value
+        // then parse the string to float
+        // finally unwrap the result
+        let price:f32 = item["price_usd"].to_string().parse().unwrap();
+
+        // a bit about string formatting, very useful
+        // from https://doc.rust-lang.org/std/fmt/#syntax
+        println!("{: >2} {: <17} {:10.4}", item["rank"], item["name"], price);
+    }
 }
